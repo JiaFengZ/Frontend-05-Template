e5b429f30e785030713747e3ad220acb4010df07
unknown <1040185058@qq.com>
2020-11-30 01:06:46 +0800

    fix error

--- Week 08/server.js
+++ Week 08/server.js
@@ -7,13 +7,13 @@ http.createServer((request, response) => {
   }).on('data', chunk => {
     body.push(chunk.toString())
   }).on('end', () => {
-    body = Buffer.concat(body).toString()
-    console.log('body:' + body)
+    //body = Buffer.concat(body).toString()
+    //console.log('body:' + body)
     response.writeHead(200, {
       'Content-Type': 'text/html'
     })
     response.end(' Hello World\n')
   })
-}).listen('8080')
+}).listen(8080)
 
 console.log('server started')
