54253ddfe3999515442955b218d6cfc5b6dac624
unknown <1040185058@qq.com>
2020-12-13 23:54:53 +0800

    layout

---
+++ Week 10/layout.js
@@ -0,0 +1,349 @@
+function layout(element) {
+  if (!element) {
+    return
+  }
+  let elementStyle = getStyle(element)
+  if (elementStyle.display !== 'flex') { // 目前仅处理flex布局
+    return
+  }
+
+  let items = element.children.filter(e => e.type === 'element')
+  items.sort((a, b) => {
+    return (a.order || 0) - (b.order || 0)
+  })
+
+  let style = elementStyle
+
+  ['width', 'height'].forEach(size => {
+    if (style[size] === 'auto' || style[size] === '') {
+      style[size] = null
+    }
+  })
+
+  if (!style.flexDirection || style.flexDirection === 'auto') {
+    style.flexDirection = 'row'
+  }
+  if (!style.alignItems || style.alignItems === 'auto') {
+    style.alignItems = 'stretch'
+  }
+  if (!style.justifyContent || style.justifyContent === 'auto') {
+    style.justifyContent = 'flex-start'
+  }
+  if (!style.flexWrap || style.flexWrap === 'auto') {
+    style.flexWrap = 'nowrap'
+  }
+  if (!style.alignContent || style.alignContent === 'auto') {
+    style.alignContent = 'stretch'
+  }
+
+  let mainSize // 主轴尺寸
+  let mainStart // 主轴左缘位置
+  let mainEnd // 主轴右缘位置
+  let mainSign // 属性递增方向
+  let mainBase // 属性初始值
+  let crossSize // 交叉轴，同上
+  let crossStart
+  let corssEnd
+  let crossSign
+  let crossBase
+
+  if (style.flexDirection === 'row') {
+    mainSign = 'width'
+    mainStart = 'left'
+    mainEnd = 'right'
+    mainSign = +1
+    mainBase = 0
+
+    crossSize = 'height'
+    crossStart = 'top'
+    corssEnd = 'bottom'
+  }
+  if (style.flexDirection === 'row-reverse') {
+    mainSign = 'width'
+    mainStart = 'right'
+    mainEnd = 'left'
+    mainSign = -1
+    mainBase = style.width
+
+    crossSize = 'height'
+    crossStart = 'top'
+    corssEnd = 'bottom'
+  }
+  if (style.flexDirection === 'column') {
+    mainSign = 'height'
+    mainStart = 'top'
+    mainEnd = 'bottom'
+    mainSign = +1
+    mainBase = 0
+
+    crossSize = 'width'
+    crossStart = 'left'
+    corssEnd = 'right'
+  }
+  if (style.flexDirection === 'column-reverse') {
+    mainSign = 'height'
+    mainStart = 'bottom'
+    mainEnd = 'top'
+    mainSign = -1
+    mainBase = style.height
+
+    crossSize = 'width'
+    crossStart = 'left'
+    corssEnd = 'right'
+  }
+  if (style.flexWrap === 'wrap-reverse') {
+    let tmp = crossStart
+    crossStart = corssEnd
+    corssEnd = tmp
+    crossSign = -1
+  } else {
+    crossBase = 0
+    crossSign = +1
+  }
+
+  let isAutoMainSize = false
+  if (!style[mainSize]) { // 父元素没设置尺寸，由子元素撑开
+    elementStyle[mainSize] = 0
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i]
+      let itemStyle = getStyle(item)
+      if (itemStyle[mainSize] !== null || itemStyle[mainSize] !== undefined) {
+        elementStyle[mainSize] += itemStyle[mainSize]
+      }
+    }
+    isAutoMainSize = true
+  }
+
+  let flexLine = [] // 单行
+  let flexLines = [flexLine] // 多行
+
+  let mainSpace = elementStyle[mainSize]
+  let crossSpace = 0
+
+  for (let i = 0; i < items.length; i++) {
+    let item = items[i]
+    let itemStyle = getStyle(item)
+    if (itemStyle[mainSize] === null) {
+      itemStyle[mainSize] = 0
+    }
+
+    if (itemStyle.flex) {
+      flexLine.push(item)
+    } else if (style.flexWrap === 'nowrap' && isAutoMainSize) {
+      mainSpace -= itemStyle[mainSize]
+      if (itemStyle[crossSize] !== null && itemStyle[crossSize] !== undefined) {
+        crossSpace = Math.max(crossSpace, itemStyle[crossSize]) // 交叉轴尺寸由最大尺寸的元素决定
+      }
+      flexLine.push(item)
+    } else {
+      if (itemStyle[mainSize] > style[mainSize]) {
+        itemStyle[mainSize] = style[mainSize]
+      }
+      if (mainSpace < itemStyle[mainSize]) { // 换行
+        flexLine.mainSpace = mainSpace
+        flexLine.crossSpace = crossSpace
+        flexLine = [item]
+        flexLines.push(flexLine)
+        mainSpace = style[mainSize]
+        crossSpace = 0
+      } else {
+        flexLine.push(item)
+      }
+      if (itemStyle[crossSize] !== nul && itemStyle[crossSize] !== undefined) {
+        crossSpace = Math.max(crossSpace, itemStyle[crossSize])
+      }
+      mainSpace -= itemStyle[mainSize]
+    }
+  }
+  flexLine.mainSpace = mainSpace
+
+  console.log(items)
+
+  // 计算主轴---------------
+  if (style.flexWrap === 'nowrap' || isAutoMainSize) {
+    flexLine.crossSpace = (style[crossSize] !== undefined) ? style[crossSize] : crossSpace
+  } else {
+    flexLine.crossSpace = crossSpace
+  }
+
+  if (mainSpace < 0) {
+    // 尺寸不足时，按比例缩小元素
+    let scale = style[mainSize] / (style[mainSize - mainSpace])
+    let currentMain = mainBase
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i]
+      let itemStyle = getStyle(item)
+
+
+      if (item.flex) {
+        itemStyle[mainSize] = 0
+      }
+
+      itemStyle[mainSize] = itemStyle[mainSize] * scale
+      itemStyle[mainStart] = currentMain
+      itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize]
+      currentMain = itemStyle[mainEnd]
+    }
+  } else {
+    // 处理每一行
+    flexLines.forEach(items => {
+      let mainSpace = items.mainSpace
+      let flexTotal = 0
+
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i]
+        let itemStyle = getStyle(item)
+
+        if ((itemStyle.flex !== null) && itemStyle.flex !== undefined) {
+          flexTotal += itemStyle.flex
+          continue
+        }
+      }
+
+      if (flexTotal > 0) {
+        let currentMain = mainBase
+        for (let i = 0; i < items.length; i++) {
+          let item = items[i]
+          let itemStyle = getStyle(item)
+          if (itemStyle.flex) {
+            itemStyle[mainSize] = (mainSpace / flexTotal) * itemStyle.flex
+          }
+          itemStyle[mainStart] = currentMain
+          itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize]
+          currentMain = itemStyle[mainEnd]
+        }
+      } else {
+        let currentMain
+        let step
+        if (style.justifyContent === 'flex-start') {
+          currentMain = mainBase
+          step = 0
+        }
+        if (style.justifyContent === 'flex-end') {
+          currentMain = mainSpace + mainSign + mainBase
+          step = 0
+        }
+        if (style.justifyContent === 'center') {
+          currentMain = mainSpace / 2 * mainSign + mainBase
+          step = 0
+        }
+        if (style.justifyContent === 'space-between') {
+          currentMain = mainSpace / (items.length - 1) * mainSign
+          step = mainBase
+        }
+        if (style.justifyContent === 'space-around') {
+          currentMain = mainSpace / items.length * mainSign
+          step = step / 2 + mainBase
+        }
+
+        for (let i = 0; i < items.length; i++) {
+          let item = items[i]
+          let itemStyle = getStyle(item)
+          itemStyle[mainStart] = currentMain
+          itemStyle[mainEnd] = itemStyle[mainStart] + mainSign * itemStyle[mainSize]
+          currentMain = itemStyle[mainEnd] + step
+        }
+      }
+    })
+  }
+  // 计算主轴--------------------
+
+  // 计算交叉轴--------------------
+  let crossSpace
+  if (!style[crossSize]) {
+    crossSpace = 0
+    elementStyle[crossSize] = 0
+    for (let i = 0; i < flexLines.length; i++) {
+      elementStyle[crossSize] = elementStyle[crossSize] + flexLines[i].crossSpace
+    }
+  } else {
+    crossSpace = style[crossSize]
+    for (let i = 0; i < flexLines.length; i++) {
+      crossSpace -= flexLines[i].crossSpace
+    }
+  }
+
+  if (style.flexWrap === 'wrap-reverse') {
+    crossBase = style[crossSize]
+  } else {
+    crossBase = 0
+  }
+  let lineSize = style[crossSize] / flexLines.length
+  let type
+  let step
+  if (style.alignContent === 'fex-start') {
+    crossBase += 0
+    step = 0
+  }
+  if (style.alignContent === 'flex-end') {
+    crossBase += crossSign * crossSpace
+    step = 0
+  }
+  if (style.alignContent === 'center') {
+    crossBase += crossSign * crossSpace / 2
+    step = 0
+  }
+  if (style.alignContent === 'space-between') {
+    crossBase += 0
+    step = crossSpace / (flexLines.length - 1)
+  }
+  if (style.alignContent === 'space-around') {
+    step = crossSpace / (flexLines.length)
+    crossBase += crossSign * step / 2
+  }
+  if (style.alignContent === 'stretch') {
+    crossBase += 0
+    step = 0
+  }
+  flexLines.forEach(items => {
+    let lineCrossSize = style.alignContent === 'stretch' ?
+      items.crossSpace + crossSpace / flexLines.length :
+      items.crossSpace
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i]
+        let itemStyle = getStyle(item)
+        
+        let align = itemStyle.alignSelf || style.alignItems
+        if (item === null) {
+          itemStyle[crossSize] = align === 'stretch' ? lineCrossSize : 0
+        }
+        if (align === 'flex-start') {
+          itemStyle[crossStart] = crossBase
+          itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize]
+        }
+        if (align === 'flex-end') {
+          itemStyle[crossEnd] = crossBase + crossSign * lineCrossSize
+          itemStyle[crossStart] = itemStyle[crossEnd] - crossSign * itemStyle[crossSize]
+        }
+        if (align === 'center') {
+          itemStyle[crossStart] = crossBase + crossSign * (lineCrossSize - itemStyle[crossSize]) / 2
+          itemStyle[crossEnd] = itemStyle[crossStart] + crossSign * itemStyle[crossSize]
+        }
+        if (align == 'stretch') {
+          itemStyle[crossStart] = crossBase
+          //itemStyle[crossEnd] = crossBase + crossSign * ((itemStyle[crossSize] !== null && itemStyle[crossSize] !== undefined) ? '')
+        }
+      }
+      crossBase += crossSign * (lineCrossSize + step)
+  })
+  console.log(items)
+}
+
+function getStyle(element) {
+  if (!element.style) {
+    element.style = {}
+  }
+  for (let prop in element.computedStyle) {
+    let p = element.computedStylep[prop].value
+    element.style[prop] = p
+    if (element.style[prop].toString().match(/px$/)) {
+      element.style[prop] = parseInt(element.style[prop], 10)
+    }
+    if (element.style[prop].toString().match(/^[0-9\.]+$/)) {
+      element.style[prop] = parseInt(element.style[prop], 10)
+    }
+  }
+  return element.style
+}
+
+module.exports = layout
---
+++ Week 10/parser.js
@@ -0,0 +1,386 @@
+const css = require('css')
+const layout = require('./layout')
+const EOF = Symbol('EOF')
+let currentToken = null
+let currentAttribute = null
+let currentTextNode = null
+let stack = [{type: 'document', children: [], attributes: []}]
+
+// css规则暂存到一个数组里
+let rules = []
+function addCssRules(text) {
+  let ast = css.parse(text)
+  rules.push(...ast.stylesheet.rules)
+}
+
+function computeCSS(element) {
+  let elements = stack.slice().reverse()
+  if (!element.computeStyle) {
+    element.computeStyle = {}
+  }
+
+  for (let rule of rules) {
+    let selectorParts = rule.selectors[0].split(' ').reverse()
+    if (!match(element, selectorParts[0])) {
+      continue
+    }
+
+    let matched = false
+    let j = 1
+    for (let i = 0; i < elements.length; i++) {
+      if (match(elements[i], selectorParts[j])) {
+        j++
+      }
+    }
+    if (j >= selectorParts.length) { // 判断是否所有选择器都匹配上了元素
+      matched = true
+    }
+    if (matched) {
+      // 匹配到元素
+      let computedStyle = element.computeStyle
+      let sp = specificity(rule.selectors[0])
+      for (let declaration of rule.declarations) {
+        if (!computedStyle[declaration.property]) {
+          computedStyle[declaration.property] = {}
+        }
+        if (!computedStyle[declaration.property].specificity) {
+          computedStyle[declaration.property].value = declaration.value 
+          computedStyle[declaration.property].specificity = sp
+        } else if (compare(computedStyle[declaration.property].specificity, sp) < 0) {
+          computedStyle[declaration.property].value = declaration.value 
+          computedStyle[declaration.property].specificity = sp
+        }
+      }
+      //console.log(element.computeStyle)
+    }
+  }
+}
+
+function match(element, selector) {
+  if (!selector || !element) {
+    return false
+  }
+  if (selector.charAt(0) === '#') {
+    let attr = element.attributes.filter(attr => attr.name === 'id')[0]
+    if (attr && attr.value === selector.replace('#', '')) {
+      return true
+    }
+  } else if (selector.charAt(0) === '.') {
+    let attr = element.attributes.filter(attr => attr.name === 'class')[0]
+    if (attr && attr.value === selector.replace('.', '')) {
+      return true
+    }
+  } else {
+    if (element.tagName === selector) {
+      return true
+    }
+  }
+  return false
+}
+
+function specificity(selector) {
+  let p = [0, 0, 0, 0] // id, class, tagName
+  let selectorParts = selector.split(' ')
+  for (let part of selectorParts) {
+    if (part.charAt(0) === '#') {
+      p[1] += 1
+    } else if (part.charAt(0) === '.') {
+      p[2] += 1
+    } else {
+      p[3] += 1
+    }
+  }
+  return p
+}
+
+function compare(sp1, sp2) {
+  if (sp1[0] - sp2[0]) {
+    return sp1[0] - sp2[0]
+  }
+  if (sp1[1] - sp2[1]) {
+    return sp1[1] - sp2[1]
+  }
+  if (sp1[2] - sp2[2]) {
+    return sp1[2] - sp2[2]
+  }
+  return sp1[3] - sp2[3]
+}
+
+function emit(token) {
+  //console.log(token)
+  let top = stack[stack.length - 1]
+
+  if (token.type === 'startTag') {
+    let element = {
+      type: 'element',
+      children: [],
+      attributes: []
+    }
+    
+    element.tagName = token.tagName
+
+    for (let p in token) {
+      if (p !== 'type' && p !== 'tagName') {
+        // 收集属性
+        element.attributes.push({
+          name: p,
+          value: token[p]
+        })
+      }
+    }
+
+    // 计算css规则
+    computeCSS(element)
+
+    // 构建子节点和父节点关系
+    top.children.push(element)
+    element.parent = top
+
+    if (!token.isSelfClosing) {
+      stack.push(element)
+    }
+    currentTextNode = null
+  } else if (token.type === 'endTag') {
+    if (top.tagName !== token.tagName) { // 闭合标签，从堆栈中出栈
+      throw new Error('Tag start end doesn\'t match !')
+    } else {
+      if (top.tagName === 'style') { // 收集css规则
+        addCssRules(top.children[0].content)
+      }
+      layout(top)
+      stack.pop()
+    }
+    currentTextNode = null
+  } else if (token.type === 'text') {
+    if (currentTextNode === null)  {
+      currentTextNode = {
+        type: 'text',
+        content: ''
+      }
+      top.children.push(currentTextNode)
+    }
+    currentTextNode.content += token.content
+  }
+}
+
+// 初始状态
+function data(c) {
+  if (c === '<') {
+    return tagOpen
+  } else if (c === EOF) {
+    emit({
+      type: 'EOF'
+    })
+    return
+  } else {
+    emit({
+      type: 'text',
+      content: c
+    })
+    return data
+  }
+}
+
+
+function tagOpen(c) {
+  if (c === '/') {
+    return endTagOpen
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: 'startTag',
+      tagName: ''
+    }
+    return tagName(c)
+  } else {
+    return
+  }
+}
+
+function endTagOpen(c) {
+  if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: 'endTag',
+      tagName: ''
+    }
+    return tagName(c)
+  } else if (c === '>') {
+    // error
+  } else if (c === EOF) {
+    // error
+  } else {
+    // error
+  }
+}
+
+function tagName(c) {
+  if (c.match(/^[\t\n\f ]$/)) { // 制表符/换行符/禁止符/空格符
+    return beforeAttributeName
+  } else if (c === '/') { // 不带属性的自封闭标签
+    return selfClosingStartTag
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    currentToken.tagName += c
+    return tagName
+  } else if (c === '>') {
+    emit(currentToken)
+    return data
+  } else {
+    currentToken.tagName += c
+    return tagName
+  }
+}
+
+function beforeAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName
+  } else if (c === '/' || c === '>' || c === EOF) { // 带属性的自封闭标签
+    return afterAttributeName(c)
+  } else if (c === '=') { 
+    // error
+  } else {
+    currentAttribute = {
+      name: '',
+      value: ''
+    }
+    return attributeName(c)
+  }
+}
+
+function afterAttributeName(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return afterAttributeName
+  } else if (c === '/') {
+    return selfClosingStartTag
+  } else if (c === '=') {
+    return beforeAttributeValue
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data
+  } else if (c === EOF) {
+
+  } else {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    currentAttribute = {
+      name: '',
+      value: ''
+    }
+    return attributeName
+  }
+}
+
+function attributeName(c) {
+  if (c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
+    return afterAttributeName(c)
+  } else if (c === '=') {
+    return beforeAttributeValue
+  } else if (c === '\u0000') {
+    
+  } else if (c === '\'' || c === '"' || c === '<') {
+    // error
+  } else {
+    currentAttribute.name += c
+    return attributeName
+  }
+}
+
+function beforeAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/) || c === '/' || c === '>' || c === EOF) {
+    return beforeAttributeValue
+  } else if (c === '"') {
+    return doubleQuotedAttributeValue
+  } else if (c === '\'') {
+    return singleQuotedAttributeValue
+  } else if (c === '>') {
+    //return data
+  } else {
+    return UnquotedAttributeValue(c)
+  }
+}
+
+function doubleQuotedAttributeValue(c) {
+  if (c === '"') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return afterQuotedAttributeValue
+  } else if (c === '\u0000') {
+
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c
+    return doubleQuotedAttributeValue
+  }
+}
+
+function singleQuotedAttributeValue(c) {
+  if (c === '\'') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return afterQuotedAttributeValue
+  } else if (c === '\u0000') {
+
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c
+    return singleQuotedAttributeValue
+  }
+}
+
+function afterQuotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    return beforeAttributeName
+  } else if (c === '/') {
+    return selfClosingStartTag
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data
+  } else if (c === EOF) {
+
+  } else {
+    return beforeAttributeName(c)
+  }
+}
+
+function UnquotedAttributeValue(c) {
+  if (c.match(/^[\t\n\f ]$/)) {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return beforeAttributeName
+  } else if (c === '/') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    return selfClosingStartTag
+  } else if (c === '>') {
+    currentToken[currentAttribute.name] = currentAttribute.value
+    emit(currentToken)
+    return data
+  } else if (c === '\u0000') {
+
+  } else if (c === '"' || c === '\'' || c === '<' || c === '=' || c === '`') {
+
+  } else if (c === EOF) {
+
+  } else {
+    currentAttribute.value += c
+    return UnquotedAttributeValue
+  }
+}
+
+function selfClosingStartTag(c) {
+  if (c === '>') {
+    currentToken.isSelfClosing = true
+    emit(currentToken)
+    return data
+  } else if (c === EOF) {
+    // error
+  } else {
+    // error
+  }
+}
+
+module.exports.parseHtml = function parseHtml(html) {
+  let state = data
+  for (let c of html) {
+    state = state(c)
+  }
+  state = state(EOF)
+  console.log(stack[0])
+}
