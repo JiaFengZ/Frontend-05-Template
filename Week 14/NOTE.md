bc9aaed8c7dea407d8ddd61985ea040c45f46565
unknown <1040185058@qq.com>
2021-01-10 22:14:24 +0800

    note

--- Week 14/NOTE.md
+++ Week 14/NOTE.md
@@ -1,1 +1,14 @@
 学习笔记
+
+## 组件的概念
+* Properties 强调从属关系
+* Attribute 强调特性
+* Methods
+* inherit
+* config & state 组件外配置传递信息
+* Event 组件向外抛出信息
+* LifeCycle 生命周期
+  * mount
+  * update
+  * unmount
+* Children 树形结构，界面描述能力
--- Week 14/main.js
+++ Week 14/main.js
@@ -1,19 +1,80 @@
-import { createElement, Component } from './framework'
+import { createElement, Component } from "./framework"
+
+const IMAGE_WIDTH = 500
+
+const SLIDE_CON = 100
+
 class Carousel extends Component {
   constructor() {
-    super()
+    super();
     this.attributes = Object.create(null)
   }
   setAttribute(name, value) {
     this.attributes[name] = value
   }
   render() {
-    this.root = document.createElement('div')
-    for (let record of this.attributes.src) {
-      let child = document.createElement('img')
-      child.src = record
+    this.root = document.createElement("div")
+    this.root.classList.add("carousel")
+    // 生成轮播基本dom结构
+    for (const src of this.attributes.src) {
+      let child = document.createElement("div")
+      child.style.backgroundImage = `url('${src}')`
       this.root.appendChild(child)
     }
+
+    // const children = this.root.children
+    // let currentIndex = 0
+    // setInterval(() => {
+    // 	let nextIndex = (currentIndex + 1) % children.length
+    // 	let current = children[currentIndex]
+    // 	let next = children[nextIndex]
+    // 	next.style.transition = 'none'
+    // 	next.style.transform = `translateX(${SLIDE_CON - SLIDE_CON * nextIndex}%)`
+    // 	setTimeout(() => {
+    // 		next.style.transition = ''
+    // 		current.style.transform = `translateX(-${SLIDE_CON * (currentIndex + 1)}%)` // 当前图片左移一个位置
+    // 		next.style.transform = `translateX(-${SLIDE_CON * nextIndex}%)`  // 下一张图片移动到当前位置
+    // 		currentIndex = nextIndex
+    // 	}, 16)
+    // }, 2000)
+
+    const children = this.root.children
+    let position = 0
+
+    const down = (event) => {
+      const startX = event.clientX
+
+      const move = (event) => {
+        const x = event.clientX - startX
+
+        let current = position - (x - (x % IMAGE_WIDTH)) / IMAGE_WIDTH
+        for (const offset of [-1, 0, 1]) {
+          let pos = offset + current
+          pos = (pos + children.length) % children.length
+          children[pos].style.transition = 'none'
+          children[pos].style.transform = `translateX(${-pos * IMAGE_WIDTH + offset * IMAGE_WIDTH + (x % IMAGE_WIDTH)}px)`
+        }
+      }
+
+      const up = (event) => {
+        const x = event.clientX - startX
+        position = (position - Math.round(x / IMAGE_WIDTH) + children.length) % children.length
+        for (const offset of [0, -Math.sign(Math.round(x / IMAGE_WIDTH) - x + IMAGE_WIDTH / 2 * Math.sign(x))]) {
+          let pos = position + offset;
+          pos = (pos + children.length) % children.length
+          children[pos].style.transition = ''
+          children[pos].style.transform = `translateX(${-pos * IMAGE_WIDTH + offset * IMAGE_WIDTH}px)`
+
+        }
+        document.removeEventListener('mousemove', move)
+        document.removeEventListener('mouseup', up)
+      }
+
+      document.addEventListener('mousemove', move)
+      document.addEventListener('mouseup', up)
+    }
+    this.root.addEventListener('mousedown', down)
+
     return this.root
   }
   mountTo(parent) {
@@ -21,7 +82,7 @@ class Carousel extends Component {
   }
 }
 
-let d = [
+let src = [
   '/static/1.jpeg',
   '/static/2.jpeg',
   '/static/3.jpeg',
@@ -29,6 +90,6 @@ let d = [
   '/static/5.jpeg'
 ]
 
-let a = <Carousel src={d}/>
+let a = <Carousel src={src} />
 
 a.mountTo(document.body)
